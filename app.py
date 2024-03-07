5a7f602394b0eef5d4683148ab48d6b22199b81a
edFajardoGenzeon <ed.fajardo@genzeon.com>
2024-03-07 22:25:03 +0800

    Added gitignore added index.html template updated app.py to implement add, edit, delete and search routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import jsonify, make_response, request
+from flask import jsonify, make_response, request, render_template
 from library_management.functions import get_all_books, get_all_users
 from library_management import db
 from library_management import app
@@ -8,6 +8,19 @@ from library_management.models import Users,Books
 def welcome():
     return "Welcome To Genzeon Library"
 
+@app.route('/')
+def indexLibrarian():
+    global isStudent
+    isStudent=False
+    books = get_all_books()
+    return render_template('index.html', books=books)
+
+@app.route('/student')
+def indexStudent():
+    global isStudent
+    isStudent=True
+    books = get_all_books()
+    return render_template('index.html', books=books, isStudent=isStudent)
 
 @app.route("/book_added", methods=["POST"])
 def book_added():
@@ -79,6 +92,15 @@ def update_book(book_id):
     db.session.commit()
     return jsonify({"message": f"Book with ID {book_id} updated successfully"})
 
+@app.route('/add_book', methods=['POST'])
+def add_book():
+    bookName = request.form['bookName']
+    authorName = request.form['authorName']
+    new_book = Books(bookName=bookName, authorName=authorName)
+    db.session.add(new_book)
+    db.session.commit()
+    # books.append({'bookName': bookName, 'authorName': authorName})
+    return render_template('index.html', books=get_all_books())
 
 @app.route("/update_user/<int:user_id>", methods=["PUT"])
 def update_user(user_id):
@@ -103,22 +125,29 @@ def update_user(user_id):
     return jsonify({"message": f"User with ID {user_id} updated successfully"})
 
 
-@app.route("/delete_book/<int:book_id>", methods=["DELETE"])
-def delete_book(book_id):
-    book = Books.query.get(book_id)
+# @app.route("/delete_book/<int:book_id>", methods=["DELETE"])
+# def delete_book(book_id):
+#     book = Books.query.get(book_id)
 
-    if book:
-        db.session.delete(book)
-        db.session.commit()
-        return make_response(
-            {"message": f"Book with ID {book_id} deleted"},
-            200
-        )
-    else:
-        return make_response(
-            {"message": f"Book with ID {book_id} not found"},
-            404
-        )
+#     if book:
+#         db.session.delete(book)
+#         db.session.commit()
+#         return make_response(
+#             {"message": f"Book with ID {book_id} deleted"},
+#             200
+#         )
+#     else:
+#         return make_response(
+#             {"message": f"Book with ID {book_id} not found"},
+#             404
+#         )
+
+@app.route('/delete_book/<int:book_id>', methods=['GET'])
+def delete_book(book_id):
+    book_to_delete = Books.query.get_or_404(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return render_template('index.html', books=get_all_books(), isStudent=isStudent)
 
 @app.route("/delete_user/<int:user_id>", methods=["DELETE"])
 def delete_user(user_id):
@@ -170,6 +199,26 @@ def books():
     return jsonify({"books": book_list})
 
 
+@app.route('/edit_book', methods=['POST'])
+def edit_book():
+    # Fetch the book from the database
+    book = Books.query.get_or_404(request.form['editBookId'])
+
+    # Update the book details based on the form data
+    book.bookName = request.form['editBookName']
+    book.authorName = request.form['editAuthorName']
+
+    # Commit the changes to the database
+    db.session.commit()
+
+    return render_template('index.html', books=get_all_books(), isStudent=isStudent)
+
+@app.route('/search_book', methods=['GET'])
+def searchBook():
+    searchText = request.args.get('title_or_author', '')
+    searchedBooks = Books.query.filter(Books.bookName.ilike(f'%{searchText}%') | Books.authorName.ilike(f'%{searchText}%')).all()
+    # searchedBooks = searchBookFunc(searchText, books)
+    return render_template('index.html', books=get_all_books(), searchedBooks=searchedBooks, isStudent=isStudent)
 
 
 if __name__ == "__main__":
